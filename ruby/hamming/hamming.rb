24a89caebaa6b5a8f0ade3214f6d436721a590fd
Yiming Chen <dsdshcym@gmail.com>
2017-12-12 11:16:11 +0800

    add refactored solution for ruby/hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,13 +1,29 @@
 class Hamming
   def self.compute(dna_a, dna_b)
+    new(dna_a, dna_b).distance
+  end
+
+  def initialize(dna_a, dna_b)
     raise ArgumentError if dna_a.length != dna_b.length
-    dna_a.chars.zip(dna_b.chars).map do |char_a, char_b|
-      if char_a != char_b
-        1
-      else
-        0
-      end
-    end.sum
+
+    @dna_a_chars = dna_a.chars
+    @dna_b_chars = dna_b.chars
+  end
+
+  def distance
+    diff_array.sum
+  end
+
+  private
+
+  attr_reader :dna_a_chars, :dna_b_chars
+
+  def dna_pairs
+    dna_a_chars.zip(dna_b_chars)
+  end
+
+  def diff_array
+    dna_pairs.map { |a, b| a == b ? 0 : 1 }
   end
 end
 
