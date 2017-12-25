2d06a1912c1f8dc437ba2ae01d72acb6a8665b06
Yiming Chen <dsdshcym@gmail.com>
2017-12-25 15:59:47 +0800

    elixir: refactor to use Stream.unfold/2 instead of pattern matching

--- elixir/sum-of-multiples/sum_of_multiples.exs
+++ elixir/sum-of-multiples/sum_of_multiples.exs
@@ -5,16 +5,18 @@ defmodule SumOfMultiples do
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
     factors
-    |> Enum.flat_map(&multiples_up_to(&1, limit))
-    |> Enum.uniq()
+    |> Stream.flat_map(&multiples_up_to(&1, limit))
+    |> Stream.uniq()
     |> Enum.sum()
   end
 
-  defp multiples_up_to(factor, limit), do: multiples_up_to(0, factor, limit)
-  defp multiples_up_to(multiple, _factor, limit) when multiple >= limit do
-    []
-  end
-  defp multiples_up_to(multiple, factor, limit) do
-    [multiple | multiples_up_to(multiple + factor, factor, limit)]
+  defp multiples_up_to(factor, limit) do
+    Stream.unfold(
+      0,
+      fn
+        out_of_limit when out_of_limit >= limit -> nil
+        multiple when multiple < limit -> {multiple, multiple + factor}
+      end
+    )
   end
 end
