13739966111914b644d40f488bef6f663e59b791
Yiming Chen <dsdshcym@gmail.com>
2017-12-30 20:40:32 +0800

    elixir: add solution for All Your Base

---
+++ elixir/all-your-base/all-your-base.exs
@@ -0,0 +1,48 @@
+defmodule AllYourBase do
+  @doc """
+  Given a number in base a, represented as a sequence of digits, converts it to base b,
+  or returns nil if either of the bases are less than 2
+  """
+
+  @spec convert(list, integer, integer) :: list
+  def convert(digits, base_a, base_b) do
+    cond do
+      empty?(digits) -> nil
+      invalid_base?(base_a) -> nil
+      invalid_base?(base_b) -> nil
+      any_negative?(digits) -> nil
+      any_invalid?(digits, base_a) -> nil
+      true ->
+        digits
+        |> to_integer(base_a)
+        |> from_integer(base_b)
+    end
+  end
+
+  defp empty?(digits), do: Enum.empty?(digits)
+
+  defp invalid_base?(base), do: base < 2
+
+  defp any_negative?(digits) do
+    digits
+    |> Enum.any?(fn(digit) -> digit < 0 end)
+  end
+
+  defp any_invalid?(digits, base) do
+    digits
+    |> Enum.any?(fn(digit) -> digit not in (0..base - 1) end)
+  end
+
+  defp to_integer(digits, base) do
+    digits
+    |> Enum.reverse()
+    |> Enum.reduce({0, 1}, fn(digit, {result, mul}) -> {result + digit * mul, mul * base} end)
+    |> elem(0)
+  end
+
+  defp from_integer(number, base), do: _from_integer(number, base) |> Enum.reverse()
+  defp _from_integer(number, base) when number < base, do: [number]
+  defp _from_integer(number, base) do
+    [rem(number, base) | _from_integer(div(number, base), base)]
+  end
+end
