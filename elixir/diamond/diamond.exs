29587241c7a30c3f86eb07886b061da5d1b04b1b
Yiming Chen <dsdshcym@gmail.com>
2018-01-16 17:50:09 +0800

    elixir: add solution for Diamond

---
+++ elixir/diamond/diamond.exs
@@ -0,0 +1,39 @@
+defmodule Diamond do
+  @doc """
+  Given a letter, it prints a diamond starting with 'A',
+  with the supplied letter at the widest point.
+  """
+  @spec build_shape(char) :: String.t
+  def build_shape(letter) do
+    letter
+    |> build_range()
+    |> Enum.map(&line/1)
+    |> Enum.map(&pad(&1, size(letter)))
+    |> Enum.map(&add_trailing_newline/1)
+    |> Enum.join()
+  end
+
+  def build_range(?A), do: [?A]
+  def build_range(letter) when letter in (?B..?Z) do
+    Enum.concat(?A..letter, (letter-1)..?A)
+  end
+
+  def line(?A), do: "A"
+  def line(letter) do
+    ([letter] ++ List.duplicate(' ', size(letter) - 2) ++ [letter])
+    |> List.to_string()
+  end
+
+  def size(?A), do: 1
+  def size(letter), do: size(letter - 1) + 2
+
+  defp pad(string, count, padding \\ " ") do
+    string
+    |> String.pad_leading(div(String.length(string) + count, 2), padding)
+    |> String.pad_trailing(count, padding)
+  end
+
+  defp add_trailing_newline(string) do
+    "#{string}\n"
+  end
+end
---
+++ elixir/diamond/diamond_test.exs
@@ -0,0 +1,79 @@
+if !System.get_env("EXERCISM_TEST_EXAMPLES") do
+  Code.load_file("diamond.exs", __DIR__)
+end
+
+ExUnit.start
+ExUnit.configure exclude: :pending, trace: true
+
+defmodule DiamondTest do
+  use ExUnit.Case
+
+  test "letter A" do
+    shape = Diamond.build_shape(?A)
+    assert shape == "A\n"
+  end
+
+  test "letter C" do
+    shape = Diamond.build_shape(?C)
+    assert shape == """
+\s A \s
+\sB B\s
+ C   C
+\sB B\s
+\s A \s
+    """
+  end
+
+  test "letter E" do
+    shape = Diamond.build_shape(?E)
+    assert shape == """
+\s   A   \s
+\s  B B  \s
+\s C   C \s
+\sD     D\s
+ E       E
+\sD     D\s
+\s C   C \s
+\s  B B  \s
+\s   A   \s
+    """
+  end
+
+  describe "build_range/1" do
+    test "returns A..A for A" do
+      assert Diamond.build_range(?A) == 'A'
+    end
+
+    test "returns A..C..A for C" do
+      assert Diamond.build_range(?C) == 'ABCBA'
+    end
+
+    test "returns A..E..A for E" do
+      assert Diamond.build_range(?E) == 'ABCDEDCBA'
+    end
+  end
+
+  describe "line/1" do
+    test "returns \"A\" for A" do
+      assert Diamond.line(?A) == "A"
+    end
+
+    test "returns \"B B\" for B" do
+      assert Diamond.line(?B) == "B B"
+    end
+  end
+
+  describe "size/1" do
+    test "returns 1 for A" do
+      assert Diamond.size(?A) == 1
+    end
+
+    test "returns 5 for C" do
+      assert Diamond.size(?C) == 5
+    end
+
+    test "returns 9 for E" do
+      assert Diamond.size(?E) == 9
+    end
+  end
+end
