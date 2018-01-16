4b904d73feecebdc442102564d3ed0be15594258
Yiming Chen <dsdshcym@gmail.com>
2018-01-16 15:38:44 +0800

    elixir: add solution for PascalsTriangle

---
+++ elixir/pascals-triangle/pascals_triangle.exs
@@ -0,0 +1,17 @@
+defmodule PascalsTriangle do
+  @doc """
+  Calculates the rows of a pascal triangle
+  with the given height
+  """
+  @spec rows(integer) :: [[integer]]
+  def rows(num) do
+    (1..num)
+    |> Enum.map(&row/1)
+  end
+
+  defp row(1), do: [1]
+  defp row(num) do
+    [1 |
+     row(num - 1) |> Enum.chunk_every(2, 1) |> Enum.map(&Enum.sum/1)]
+  end
+end
