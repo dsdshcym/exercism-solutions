99903bfde1db719f407f80f14614ce0d8848924a
Yiming Chen <dsdshcym@gmail.com>
2018-01-25 21:54:31 +0800

    elixir: refactor Palindrome Products a little bit
    - See also http://exercism.io/submissions/4938e174ba9f46f18f309e395a2ef690

--- elixir/palindrome-products/palindrome_products.exs
+++ elixir/palindrome-products/palindrome_products.exs
@@ -4,13 +4,15 @@ defmodule Palindromes do
   """
   @spec generate(non_neg_integer, non_neg_integer) :: map
   def generate(max_factor, min_factor \\ 1) do
+    palindrome_factors(min_factor, max_factor)
+    |> Enum.group_by(fn [i, j] -> i * j end)
+  end
+
+  defp palindrome_factors(min_factor, max_factor) do
     for i <- min_factor..max_factor,
         j <- i..max_factor,
-        product = i * j,
-        palindrome?(product) do
-      {product, [i, j]}
-    end
-    |> Enum.group_by(fn {product, _} -> product end, fn {_, factors} -> factors end)
+        palindrome?(i * j),
+        do: [i, j]
   end
 
   defp palindrome?(number) when is_integer(number) do
