2620ce59024327e835108bbac6d662780333f734
Yiming Chen <dsdshcym@gmail.com>
2018-01-26 15:33:38 +0800

    elixir: refactor to reflect the core logic of Rail Fence Cipher

--- elixir/rail-fence-cipher/rail_fence_cipher.exs
+++ elixir/rail-fence-cipher/rail_fence_cipher.exs
@@ -4,14 +4,17 @@ defmodule RailFenceCipher do
   """
   @spec encode(String.t(), pos_integer) :: String.t()
   def encode(str, rails) do
-    fence = fence(rails)
-
     str
-    |> String.graphemes()
-    |> Enum.zip(fence)
-    |> Enum.sort_by(fn {_char, fence} -> fence end)
-    |> Enum.map(fn {char, _fence} -> char end)
-    |> Enum.join()
+    |> transpose(encode_rule(str, rails))
+  end
+
+  defp encode_rule(str, rails) do
+    rails
+    |> fence()
+    |> Enum.take(String.length(str))
+    |> Enum.with_index()
+    |> Enum.group_by(fn {fence, _index} -> fence end, fn {_fence, index} -> index end)
+    |> Enum.flat_map(fn {_fence, indices} -> indices end)
   end
 
   @doc """
@@ -19,16 +22,13 @@ defmodule RailFenceCipher do
   """
   @spec decode(String.t(), pos_integer) :: String.t()
   def decode(str, rails) do
-    fence = fence(rails)
+    str
+    |> transpose(decode_rule(str, rails))
+  end
 
-    1..String.length(str)
-    |> Enum.zip(fence)
-    |> Enum.sort_by(fn {_index, fence} -> fence end)
-    |> Enum.map(fn {index, _fence} -> index end)
-    |> Enum.zip(str |> String.graphemes())
-    |> Enum.sort()
-    |> Enum.map(fn {_index, char} -> char end)
-    |> Enum.join()
+  defp decode_rule(str, rails) do
+    encode_rule(str, rails)
+    |> revert()
   end
 
   defp fence(1), do: [1] |> Stream.cycle()
@@ -37,5 +37,18 @@ defmodule RailFenceCipher do
     1..rails
     |> Enum.concat((rails - 1)..2)
     |> Stream.cycle()
+  end
+
+  defp transpose(str, rule) do
+    rule
+    |> Enum.map(&String.at(str, &1))
+    |> Enum.join()
+  end
+
+  defp revert(rule) do
+    rule
+    |> Enum.with_index()
+    |> Enum.sort()
+    |> Enum.map(fn {index, position} -> position end)
   end
 end
