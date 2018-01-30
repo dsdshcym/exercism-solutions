9765774ae4d1764c31be4aa75c2fb977ecdacd03
Yiming Chen <dsdshcym@gmail.com>
2018-01-30 13:01:58 +0800

    elixir: add solution to Difference of Squares

---
+++ elixir/difference-of-squares/difference_of_squares.exs
@@ -0,0 +1,35 @@
+defmodule Squares do
+  @moduledoc """
+  Calculate sum of squares, square of sums, difference between two sums from 1 to a given end number.
+  """
+
+  @doc """
+  Calculate sum of squares from 1 to a given end number.
+  """
+  @spec sum_of_squares(pos_integer) :: pos_integer
+  def sum_of_squares(number) do
+    1..number
+    |> Enum.map(&square/1)
+    |> Enum.reduce(&+/2)
+  end
+
+  @doc """
+  Calculate square of sums from 1 to a given end number.
+  """
+  @spec square_of_sums(pos_integer) :: pos_integer
+  def square_of_sums(number) do
+    1..number
+    |> Enum.reduce(&+/2)
+    |> square()
+  end
+
+  @doc """
+  Calculate difference between sum of squares and square of sums from 1 to a given end number.
+  """
+  @spec difference(pos_integer) :: pos_integer
+  def difference(number) do
+    square_of_sums(number) - sum_of_squares(number)
+  end
+
+  defp square(number), do: number * number
+end
